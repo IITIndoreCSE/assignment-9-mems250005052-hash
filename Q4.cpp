cd9bbd4a3b47815979c492962ee3a53b6dfe1104
mems250005052-hash <mems250005052@iiti.ac.in>
2025-10-17 17:12:48 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,1 +1,34 @@
+int main() {
+    SalesRecord yearSales[12];
 
+    
+    cout << "Enter sales data for 12 months:\n";
+    for (int i = 0; i < 12; ++i) {
+        cout << "Month " << i + 1 << " name: ";
+        cin >> yearSales[i].month;
+        cout << "Sales amount for " << yearSales[i].month << ": ";
+        cin >> yearSales[i].amount;
+    }
+
+    
+    int maxIndex = 0;
+    int minIndex = 0;
+
+    
+    for (int i = 1; i < 12; ++i) {
+        if (yearSales[i].amount > yearSales[maxIndex].amount) {
+            maxIndex = i;
+        }
+        if (yearSales[i].amount < yearSales[minIndex].amount) {
+            minIndex = i;
+        }
+    }
+
+    
+    cout << "\nMonth with maximum sales: " << yearSales[maxIndex].month
+         << " (" << yearSales[maxIndex].amount << ")\n";
+    cout << "Month with minimum sales: " << yearSales[minIndex].month
+         << " (" << yearSales[minIndex].amount << ")\n";
+
+    return 0;
+}
